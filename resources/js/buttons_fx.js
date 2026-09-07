e70f8e71231d939cda561da90dc62b13b353bbad
Niko <54240096+Nik0-0@users.noreply.github.com>
2026-09-07 19:37:14 +0200

    fix: remove useless inputs

--- resources/js/buttons_fx.js
+++ resources/js/buttons_fx.js
@@ -1,5 +1,5 @@
 document.addEventListener("mousemove", (e) => {
-  const selector = "button, a, [data-glow], .glow-card, .fi-input-wrp, .fi-badge, .fi-dropdown-panel, input, select, textarea";
+  const selector = "button, a, [data-glow], .glow-card, .fi-input-wrp, .fi-badge, input, select, textarea";
   let target = e.target.closest(selector);
   if (!target) return;
 
@@ -34,7 +34,7 @@ document.addEventListener("mousemove", (e) => {
       borderRadius: `calc(${borderRadius} + ${offsetSize})`,
       pointerEvents: "none",
       opacity: "0",
-      zIndex: "5",
+      zIndex: "1",
       transition: "opacity 0.3s ease",
       background:
         "radial-gradient(280px circle at var(--x, 0px) var(--y, 0px), rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0.05) 60%, transparent 100%)",
